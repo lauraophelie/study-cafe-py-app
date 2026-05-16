340f4afa627bc8a05896e23070252cc65814e85f
Laura Ophélie <lauraophelie1@gmail.com>
2026-05-17 02:02:30 +0300

    Add function to generate link via symmetric cryptography w/ fernet

--- actions/study_link.py
+++ actions/study_link.py
@@ -1,3 +1,7 @@
+import datetime
+from cryptography.fernet import Fernet
+import json
+
 class StudyLink:
     def __init__(self, socket, student_name, session_id):
         self.socket = socket
@@ -5,7 +9,31 @@ class StudyLink:
         self.session_id = session_id
     
     def generate_link(self):
-        pass
+        hostname = self.socket.gethostname()
+        addr_ip = self.socket.gethostbyname(hostname)
+        current_datetime = datetime.datetime.now()
+
+        secret_key = Fernet.generate_key()
+        fernet_key = Fernet(secret_key)
+
+        link_data = {
+            "user_id": self.student_name,
+            "session_id": self.session_id,
+            "server_host": hostname,
+            "server_ip": addr_ip,
+            "date_time_creation": current_datetime
+        }
+        study_link_data = self.process_study_link(link_data)
+        study_link = fernet_key.encrypt(study_link_data)
+
+        return study_link
+    
+    def process_study_link(self, link_data):
+        link_data_json = json.dumps(link_data)
+        link_data_encode = link_data_json.encode('utf-8')
+
+        return link_data_encode
+
 
     def save_study_session(self):
         pass
--- sockets/client_socket.py
+++ sockets/client_socket.py
@@ -1,8 +1,16 @@
 import socket
 
-sock_clt = socket.socket()
-port = 8124
+# sock_clt = socket.socket()
+# port = 8124
 
-sock_clt.connect(('127.0.0.1', port))
-print(sock_clt.recv(1024).decode())
-sock_clt.close()
+# sock_clt.connect(('127.0.0.1', port))
+# print(sock_clt.recv(1024).decode())
+# sock_clt.close()
+
+def create_client_socket():
+    return socket.socket()
+
+def connect_client_to_server(hostname, port):
+    sock_clt = create_client_socket()
+    sock_clt.connect((hostname, port))
+    sock_clt.close
