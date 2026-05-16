505a08dcc63098868f56af0ea757707a68b2d6d5
Laura Ophélie <lauraophelie1@gmail.com>
2026-05-17 00:29:10 +0300

    study link class

---
+++ actions/study_link.py
@@ -0,0 +1,11 @@
+class StudyLink:
+    def __init__(self, socket, student_name, session_id):
+        self.socket = socket
+        self.student_name = student_name
+        self.session_id = session_id
+    
+    def generate_link(self):
+        pass
+
+    def save_study_session(self):
+        pass
