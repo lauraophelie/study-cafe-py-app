35d8261aa9b1c946fcf11eaa4e4a815c2e581cd6
Laura Ophélie <lauraophelie1@gmail.com>
2026-05-18 23:25:11 +0300

    Feature : generate a derived key & sending the secret key securely

--- actions/ecdh_key.py
+++ actions/ecdh_key.py
@@ -1,5 +1,26 @@
 from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives import serialization
+from cryptography.fernet import Fernet
+
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+
+import base64
+
+def get_encrypted_group_key(secure_channel, group_key):
+    return secure_channel.encrypt(group_key)
+
+def get_derived_key(host_shared_secret):
+    derived_key = HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=None,
+        info=b"study-session"
+    ).derive(host_shared_secret)
+
+    fernet_key = base64.urlsafe_b64encode(derived_key)
+    secure_channel = Fernet(fernet_key)
+
+    return secure_channel
 
 def save_public_key(private_key, public_key_name):
     pem_public_key = private_key.public_key().public_bytes(
