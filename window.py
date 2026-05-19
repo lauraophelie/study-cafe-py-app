e10ce14fc45790f3a74f738e74a50fe22552aa04
Laura Ophélie <lauraophelie1@gmail.com>
2026-05-20 01:51:57 +0300

    Menu screen base

--- window.py
+++ window.py
@@ -1,5 +1,6 @@
 import pygame
 from game_objects.background import Map
+from game_objects.sprite import Sprite
 
 pygame.init()
 
@@ -29,6 +30,23 @@ sprites = [
 ]
 window_map = Map(f"{window_map_path}menu_map.csv", sprites, tile_size=64)
 
+all_objects_sprites = pygame.sprite.Group()
+
+ui_sprites_path = "assets/ui/"
+
+logo_app = pygame.image.load(f"{ui_sprites_path}study-cafe-logo.png").convert_alpha()
+
+start_button = pygame.image.load(f"{ui_sprites_path}start_button.png").convert_alpha()
+join_button = pygame.image.load(f"{ui_sprites_path}join_button.png").convert_alpha()
+exit_button = pygame.image.load(f"{ui_sprites_path}exit_button.png").convert_alpha()
+
+logo = Sprite(logo_app, 230, 70, "logo")
+start = Sprite(start_button, 225, 115, "start_session")
+join = Sprite(join_button, 225, 150, "join_session")
+exiting = Sprite(exit_button, 255, 195, "exit")
+
+all_objects_sprites.add(logo, start, join, exiting)
+
 run = True
 clock = pygame.time.Clock()
 
@@ -38,6 +56,8 @@ while run:
             run = False
     
     window_map.render(window)
+    all_objects_sprites.draw(window)
+
     pygame.display.flip()
     clock.tick(60)
 
