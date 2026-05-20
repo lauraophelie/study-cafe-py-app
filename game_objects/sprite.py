9cdf1942c586037767985f2005ac769eeca5992d
Laura Ophélie <lauraophelie1@gmail.com>
2026-05-20 15:44:42 +0300

    Handle mouse event

--- game_objects/sprite.py
+++ game_objects/sprite.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, sprite_image, position_x, position_y, sprite_name=""):
+    def __init__(self, sprite_image, position_x, position_y, sprite_name="", callback=None):
         super().__init__()
         self.image = sprite_image
         self.rect = self.image.get_rect()
@@ -9,7 +9,10 @@ class Sprite(pygame.sprite.Sprite):
         self.rect.x = position_x
         self.rect.y = position_y
         self.name = sprite_name
+        self.callback = callback
 
-    def update(self):
-        pass
+    def update(self, events):
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.callback()
 
--- window.py
+++ window.py
@@ -40,9 +40,15 @@ start_button = pygame.image.load(f"{ui_sprites_path}start_button.png").convert_a
 join_button = pygame.image.load(f"{ui_sprites_path}join_button.png").convert_alpha()
 exit_button = pygame.image.load(f"{ui_sprites_path}exit_button.png").convert_alpha()
 
+def create_session():
+    print("create study session")
+
+def join_study_session():
+    print("join study session")
+
 logo = Sprite(logo_app, 230, 70, "logo")
-start = Sprite(start_button, 225, 115, "start_session")
-join = Sprite(join_button, 225, 150, "join_session")
+start = Sprite(start_button, 225, 115, "start_session", create_session)
+join = Sprite(join_button, 225, 150, "join_session", join_study_session)
 exiting = Sprite(exit_button, 255, 195, "exit")
 
 all_objects_sprites.add(logo, start, join, exiting)
